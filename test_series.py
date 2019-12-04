e601ae9744421ce0ed975bf28bada1c36dd5e6e5
JCode1986 <joseph.hangarter@yahoo.com>
2019-12-03 16:29:29 -0800

    fibonacci and lucas completed, same with testing

---
+++ test_series.py
@@ -0,0 +1,62 @@
+from series import fibonacci, lucas
+import pytest
+
+# Fib Test
+#first index
+def test_one():
+  expected = 0
+  actual = fibonacci(0)
+  assert actual == expected
+
+#second index
+def test_two():
+  expected = 1
+  actual = fibonacci(1)
+  assert actual == expected
+
+#third index
+def test_three():
+  expected = 1
+  actual = fibonacci(2)
+  assert actual == expected
+
+#twentieth index
+def test_four():
+  expected = 6765
+  actual = fibonacci(20)
+  assert actual == expected
+
+#less than 0
+def test_five():
+  expected = None
+  actual = fibonacci(-1)
+  assert actual == expected
+
+#failed as expected
+def test_six():
+  with pytest.raises(TypeError):  # Pass in the expected error
+    fibonacci('String')
+
+#lucas test
+#first index
+def test_seven():
+  expected = 2
+  actual = lucas(0)
+  assert actual == expected
+
+#second index
+def test_eight():
+  expected = 1
+  actual = lucas(1)
+  assert actual == expected
+
+#ninth index
+def test_nine():
+  expected = 76
+  actual = lucas(9)
+  assert actual == expected
+
+#failed as expected
+def test_ten():
+  with pytest.raises(TypeError):
+    fibonacci('String')
